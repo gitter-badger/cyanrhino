f781117bbf13145bae51646ae481f367d77ff0fa
ishida <webmaster@joe1cafe.com>
2014-09-30 23:26:59 +0800

    repair mongo lock

--- a.bat
+++ a.bat
@@ -1,1 +1,4 @@
-@echo off mongod --dbpath="C:\Program Files\MongoDB 2.6 Standard\data\db"
+@echo off 
+del "C:\Program Files\MongoDB 2.6 Standard\data\db\mongod.lock"
+mongod --repair
+mongod --dbpath="C:\Program Files\MongoDB 2.6 Standard\data\db" --port=27017 --logpath="C:\Program Files\MongoDB 2.6 Standard\data\db\mongo.log"
